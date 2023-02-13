51dcc40998fef0637d9545eea29ece2cf7cc8036
yamoo9 <yamoo9@naver.com>
2023-02-13 16:05:24 +0900

    learn. 유사하게 구현한 가상 DOM 사용법(React와 유사)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,13 @@
 import './utils/supportJavaScript.js';
 // import './vanilla.script.js';
-import './react.script.js';
+// import './react.script.js';
 
+import { createElement as h, render } from './likeReact/index.js';
 
-// console.log('React 개발 시작하기! 😊');
+const app = h('div', {
+  className: 'app'
+}, h('h1', null, 'Hello Virtual DOM'));
+
+render(app, document.getElementById('root'));
+
+console.log(app);
