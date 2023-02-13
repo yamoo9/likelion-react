ae5bdf8cf39cfa04e66c45f182772db5e65fdda0
yamoo9 <yamoo9@naver.com>
2023-02-14 07:54:33 +0900

    refactor. 가상 DOM 구현(모방) 예시

--- frontend/src/learn/likereact.script.js
+++ frontend/src/learn/likereact.script.js
@@ -1,7 +1,18 @@
 import { createElement as h, render } from './likeReact/index.js';
 
-const app = h('div', {
-  className: 'app'
-}, h('h1', null, 'Hello Virtual DOM'));
+const app = h(
+  // 타입(type)
+  'div', 
+  // 프롭스(props)
+  { className: 'app', lang: 'en' }, 
+  // ...칠드런(children)
+  h('h1', null, 'Hello Virtual ', h('abbr', { title: 'Document Object Model' }, 'DOM')),
+  h('p', null, `The virtual DOM (VDOM) is a programming concept where an ideal, or “virtual”, representation of a UI is kept in memory and synced with the “real” DOM by a library such as ReactDOM.`),
+);
 
-render(app, document.getElementById('root'));
+render(
+  // 가상 DOM 요소(virtual element)
+  app, 
+  // 실제 DOM 요소(actual element)
+  document.getElementById('root')
+);
