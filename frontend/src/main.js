bf90c8aa99bcde08ac2a1bbf8f8776cc964c1a86
yamoo9 <yamoo9@naver.com>
2023-02-13 14:50:52 +0900

    learn. React CDN으로 시작하기

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,48 +1,6 @@
 import './utils/supportJavaScript.js';
 // import './vanilla.script.js';
+import './react.script.js';
 
-// 명령형 프로그래밍
-// const root = document.getElementById('root');
-// const headline = root.querySelector('h1');
 
-// document.addEventListener('click', () => {
-//   headline.textContent = 'update headline content';
-// });
-
-// React가 제공
-// 선언형 프로그래밍
-
-// 범쌤 JavaScript
-// 바닐라 프로젝트 (명령형 프로그래밍)
-import { createElement, state, render, update } from './vanilla.script.js';
-
-const app = createElement(
-  'section', 
-  {
-    'class': 'app',
-    'aria-label': 'Like React App'
-  }, 
-  createElement('h1', {}, 'hello App'),
-  createElement('p', {}, 'blab blab blab'),
-  createElement('ul', {}, 
-    createElement('li', {}, 'item 1'),
-    createElement('li', {}, 'item 2'),
-    createElement('li', {}, 'item 3'),
-  ),
-);
-
-document.getElementById('root').append(app);
-
-// console.log(state);
-
-// render(state);
-
-document.addEventListener('click', () => {
-  update({
-    subjects: 'React는 선언형 프로그래밍 방식을 사용합니다.'
-  });
-
-  // console.log(state);
-});
-
-// console.log('React 개발 시작하기! 😊');
+console.log('React 개발 시작하기! 😊');
---
+++ frontend/src/react.script.js
@@ -0,0 +1,20 @@
+
+
+const divElement = document.createElement('div');
+console.dir(divElement);
+
+// divElement.setAttribute('id', attributeValue);
+// divElement.setAttribute('title', attributeValue);
+// divElement.setAttribute('class', attributeValue);
+// divElement.setAttribute('aria-label', attributeValue);
+
+const articleReactElement = React.createElement('article');
+
+// DOM
+// 실제 문서 객체 모델
+
+// Virtual DOM
+// 실제의 것을 추상화 한 것 
+// 가상 문서 객체 모델
+
+console.log(articleReactElement);
