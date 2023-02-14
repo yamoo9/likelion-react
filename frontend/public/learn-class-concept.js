f0dc53e48a6e61a1c21ae61fbb568101db9e7c6b
yamoo9 <yamoo9@naver.com>
2023-02-14 11:23:47 +0900

    learn. ES 클래스 개념 학습

---
+++ frontend/public/learn-class-concept.js
@@ -0,0 +1,39 @@
+// 유틸리티 함수
+function getRandomNumber(n) {
+  return Math.round(Math.random() * n);
+}
+
+function generateUniqueId() {
+  const characters = 'abcdefghijklmn'.split('');
+  let key = '';
+  characters.forEach((c) => {
+    key += characters[getRandomNumber(characters.length - 1)];
+  });
+  return `euid-${key}`;
+}
+
+// 클래스 문법
+class Event {
+  // 비공개(private) 필드
+  // 접근 제어자
+  #uid = '';
+
+  // 생성자(constructor)
+  constructor(type, date) {
+    // this는 생성된 Event 클래스의 인스턴스(객체)를 말함
+    this.type = type;
+    this.date = date;
+    this.#uid = generateUniqueId();
+  }
+
+  // 인스턴스(instance) : 생성된 객체
+  // 생성된 객체만 사용 가능한 메서드
+  // 인스턴스 메서드
+  getType() {
+    return this.type;
+  }
+
+  getUid() {
+    return this.#uid;
+  }
+}
