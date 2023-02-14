f3016ea4dfb06453423177061cb181edb34caa4c
yamoo9 <yamoo9@naver.com>
2023-02-14 09:50:29 +0900

    learn. React 컴포넌트 vs. React 엘리먼트

--- frontend/src/learn/react.components.js
+++ frontend/src/learn/react.components.js
@@ -2,14 +2,23 @@
 /* React Component Types                                                      */
 /* -------------------------------------------------------------------------- */
 
-// 1. class Component
-// 클래스 문법을 사용해 React 요소를 여러번 생성할 수 있는 컴포넌트 설계
-class {
+// Component vs. Element
+// 엘리먼트 Element → <div></div>
+// 컴포넌트(구성요소) Component → HTMLDivElement class
 
+// React 컴포넌트의 역할
+// - 재사용
+// - React 엘리먼트 생성 (React.createElement)
+// - 생성된 React 엘리먼트 반환(return)
+
+// 1. functional Component
+// 함수 문법을 사용해 React 요소를 여러 번 생성할 수 있는 컴포넌트 설계
+export function Group() {
+  return React.createElement('div', { role: 'group' }); // <div role="group"></div>
 }
 
-// 2. functional Component
-// 함수 문법을 사용해 React 요소를 여러번 생성할 수 있는 컴포넌트 설계
-function () {
-  
-}
+// 2. class Component
+// 클래스 문법을 사용해 React 요소를 여러 번 생성할 수 있는 컴포넌트 설계
+// class {
+
+// }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,2 +1,13 @@
 // React 컴포넌트 vs 엘리먼트
-import './learn/react.components.js';
+import { Group } from './learn/react.components.js';
+
+// 함수 타입
+// React 월드
+// React 엘리먼트 생성 과정에서는 반드시 React.createElement API
+
+// 사용법 1
+// 웹브라우저가 인식 가능한 HTML 표준 엘리먼트 이름 (문자 값)
+console.log(React.createElement('details'));
+// 사용법 2
+// 컴포넌트 함수 또는 클래스 참조 전달 
+console.log(React.createElement(Group));
