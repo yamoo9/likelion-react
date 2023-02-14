8d6867be89a4272692482ab541473534490de6d7
yamoo9 <yamoo9@naver.com>
2023-02-14 15:23:46 +0900

    learn. React 컴포넌트 타입 별 Props 사용 방법

--- frontend/public/demo.js
+++ frontend/public/demo.js
@@ -1,13 +1,10 @@
 /* React 컴포넌트 --------------------------------------------------------------- */
 
+// JSX 보간법(interpolation)
+// <element prop={value}>this is {content}</element>
+
 // 함수(형) 컴포넌트
 function Group(props) {
-  console.log(props.lang);
-  console.log(props.content);
-
-  // JSX 보간법(interpolation)
-  // <element prop={value}>this is {content}</element>
-
   return (
     <div role="group" lang={props.lang}>
       {props.content}
@@ -17,12 +14,23 @@ function Group(props) {
 
 // 클래스 컴포넌트
 class Logo extends React.Component {
+
+  // 생성자
+  // constructor(props) {
+  //   super(props);
+  // }
+  
   // 렌더링
   render() {
+
+    // React는 99% JavaScript
+    // 구조 분해 할당
+    const { path, label } = this.props;
+    
     // React 엘리먼트 반환  
     return (
-      <img src="/assets/react-logo.svg" alt="React" />
-    )
+      <img src={path} alt={label} />
+    );
   }
 
 }
@@ -33,67 +41,13 @@ const reactDomRoot = ReactDOM.createRoot(document.getElementById('root'));
 
 reactDomRoot.render(
   <React.Fragment>
-    <Group lang="es" content="sit amet consectetur." />
+    <Logo path="/assets/react-logo.svg" label="React" />
+    <Group />
+    <Logo
+      path="https://upload.vectorlogo.zone/logos/nextjs/images/60eff509-53dd-4280-92e7-7318fa02e934.svg"
+      label="Next.js"
+      unknown="아무 것도 몰라요~"
+    />
     <Group lang="ru" content="Далеко-далеко за словесными горами." />
   </React.Fragment>
-)
-
-// reactDomRoot.render(
-//   [
-//     // with JSX
-//     <div role="presentation" lang="ko" key="with-jsx">
-//       <Logo /> 리액트
-//     </div>,
-
-//     // without JSX
-//     React.createElement(
-//       'div',
-//       { role: 'gorup', lang: 'en', key: "without-jsx" },
-//       React.createElement(Logo),
-//       'React'
-//     ),
-//   ]
-// );
-
-
-// 함수의 arguments
-function sum(x, y) {
-  return x + y;
-}
-
-sum(10, 1000); // 1010
-sum(-9, -2); // 1010
-
-// React 컴포넌트의 Props (함수의 전달인자)
-function MenuList(props) {
-  // { items: [...] }
-  return (
-    <ul>
-      <li>item 1</li>
-      <li>item 2</li>
-    </ul>
-  )
-}
-
-// React.createElement(MenuList); // sum();
-
-// sum(29, 103);
-// React.createElement(
-//   MenuList, 
-//   { 
-//     items: [ 
-//       { id: 'menu-1', title: 'munu one'},
-//       { id: 'menu-2', title: 'munu two'},
-//     ]
-//   }
-// );
-
-
-{/* <MenuList></MenuList>
-
-const items = [ 
-  { id: 'menu-1', title: 'munu one'},
-  { id: 'menu-2', title: 'munu two'},
-] */}
-
-{/* <MenuList items={items}></MenuList> */}
+);
