12ff33a3f4f205d7ac584c30f4663cc46802a39a
yamoo9 <yamoo9@naver.com>
2023-02-14 14:42:14 +0900

    learn. React 함수 컴포넌트 Props 실습

--- frontend/public/demo.js
+++ frontend/public/demo.js
@@ -1,10 +1,16 @@
 /* React 컴포넌트 --------------------------------------------------------------- */
 
 // 함수(형) 컴포넌트
-function Group() {
+function Group(props) {
+  console.log(props.lang);
+  console.log(props.content);
+
+  // JSX 보간법(interpolation)
+  // <element prop={value}>this is {content}</element>
+
   return (
-    <div role="group" lang="en">
-      Group Element
+    <div role="group" lang={props.lang}>
+      {props.content}
     </div>
   )
 }
@@ -26,17 +32,68 @@ class Logo extends React.Component {
 const reactDomRoot = ReactDOM.createRoot(document.getElementById('root'));
 
 reactDomRoot.render(
-  [
-    // with JSX
-  <div role="group" lang="en" key="with-jsx">
-    <Logo />
-  </div>,
+  <React.Fragment>
+    <Group lang="es" content="sit amet consectetur." />
+    <Group lang="ru" content="Далеко-далеко за словесными горами." />
+  </React.Fragment>
+)
 
-  // without JSX
-  React.createElement(
-    'div',
-    { role: 'gorup', lang: 'en', key: "without-jsx" },
-    React.createElement(Logo),
-  ),
-  ]
-);
+// reactDomRoot.render(
+//   [
+//     // with JSX
+//     <div role="presentation" lang="ko" key="with-jsx">
+//       <Logo /> 리액트
+//     </div>,
+
+//     // without JSX
+//     React.createElement(
+//       'div',
+//       { role: 'gorup', lang: 'en', key: "without-jsx" },
+//       React.createElement(Logo),
+//       'React'
+//     ),
+//   ]
+// );
+
+
+// 함수의 arguments
+function sum(x, y) {
+  return x + y;
+}
+
+sum(10, 1000); // 1010
+sum(-9, -2); // 1010
+
+// React 컴포넌트의 Props (함수의 전달인자)
+function MenuList(props) {
+  // { items: [...] }
+  return (
+    <ul>
+      <li>item 1</li>
+      <li>item 2</li>
+    </ul>
+  )
+}
+
+// React.createElement(MenuList); // sum();
+
+// sum(29, 103);
+// React.createElement(
+//   MenuList, 
+//   { 
+//     items: [ 
+//       { id: 'menu-1', title: 'munu one'},
+//       { id: 'menu-2', title: 'munu two'},
+//     ]
+//   }
+// );
+
+
+{/* <MenuList></MenuList>
+
+const items = [ 
+  { id: 'menu-1', title: 'munu one'},
+  { id: 'menu-2', title: 'munu two'},
+] */}
+
+{/* <MenuList items={items}></MenuList> */}
