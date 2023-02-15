e6cf6d4e50a08b70427dafb4737066f36f7c28d2
yamoo9 <yamoo9@naver.com>
2023-02-15 11:28:51 +0900

    add. App 컴포넌트 이벤트 핸들링

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,15 +1,30 @@
 class App extends React.Component {
-  state = {
-    headline: 'React Application'
-  };
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      headline: 'React Application'
+    };
+
+    this.handleChangeHeadline = this.handleChangeHeadline.bind(this);
+  }
+
+  handleChangeHeadline() {
+    this.setState({
+      headline: 'NEW HEADLINE! 😃'
+    })
+  }
 
   render() {
     return (
       <div data-component="App">
         <h1>{ this.state.headline }</h1>
+        <button type="button" onClick={this.handleChangeHeadline}>헤드라인 변경</button>
       </div>
     );
   }
+  
 }
 
 export default App;
