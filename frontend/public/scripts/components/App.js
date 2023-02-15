21edda4b89a53c2b83b354438ed1aaad101d7e2f
yamoo9 <yamoo9@naver.com>
2023-02-15 14:49:59 +0900

    learn. 클래스의 멤버 변수 활용

--- frontend/public/scripts/components/App.js
+++ frontend/public/scripts/components/App.js
@@ -2,8 +2,6 @@ function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" =
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
-function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
@@ -11,31 +9,37 @@ function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) ===
 function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 var App = /*#__PURE__*/function (_React$Component) {
   _inherits(App, _React$Component);
   var _super = _createSuper(App);
-  function App(props) {
+  function App() {
     var _this;
     _classCallCheck(this, App);
-    _this = _super.call(this, props);
-    _this.state = {
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+    _this = _super.call.apply(_super, [this].concat(args));
+    _defineProperty(_assertThisInitialized(_this), "state", {
       headline: 'React Application',
-      isDisabled: false
-    };
-    _this.handleChangeHeadline = _this.handleChangeHeadline.bind(_assertThisInitialized(_this));
-    return _this;
-  }
-  _createClass(App, [{
-    key: "handleChangeHeadline",
-    value: function handleChangeHeadline() {
-      this.setState({
+      // isDisabled: false,
+      isToggle: false
+    });
+    _defineProperty(_assertThisInitialized(_this), "originalHeadline", _this.state.headline);
+    _defineProperty(_assertThisInitialized(_this), "handleChangeHeadline", function () {
+      _this.setState({
         headline: 'NEW HEADLINE! 😃',
         isDisabled: true
       });
-    }
-  }, {
+    });
+    return _this;
+  }
+  _createClass(App, [{
     key: "render",
     value: function render() {
+      console.log(this);
       return /*#__PURE__*/React.createElement("div", {
         "data-component": "App"
       }, /*#__PURE__*/React.createElement("h1", null, this.state.headline), /*#__PURE__*/React.createElement("button", {
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,16 +1,20 @@
 class App extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      headline: 'React Application',
-      isDisabled: false,
-    };
+  // 2022 6. 표준 기술
+  // public, private 클래스 필드, 피처
+  
+  state = {
+    headline: 'React Application',
+    // isDisabled: false,
+    isToggle: false,
+  };
 
-    this.handleChangeHeadline = this.handleChangeHeadline.bind(this);
-  }
+  // 클래스에서는 이렇게 기억할 수 있는 임의의 데이터를 관리할 수 있다.
+  // 그럼 함수는? 못한다. React Hooks (useRef)
+  // 메모이제이션 패턴
+  originalHeadline = this.state.headline;
 
-  handleChangeHeadline() {
+  handleChangeHeadline = () => {
     this.setState({
       headline: 'NEW HEADLINE! 😃',
       isDisabled: true,
@@ -18,10 +22,14 @@ class App extends React.Component {
   }
 
   render() {
+    console.log(this);
+
     return (
       <div data-component="App">
         <h1>{this.state.headline}</h1>
-        <button disabled={this.state.isDisabled} type="button" onClick={this.handleChangeHeadline}>
+        <button
+          disabled={this.state.isDisabled}
+          type="button" onClick={this.handleChangeHeadline}>
           헤드라인 업데이트
         </button>
       </div>
