523085669067e1f85682413a711d7a2b2d0a0f92
yamoo9 <yamoo9@naver.com>
2023-02-16 06:51:49 +0900

    refactor. 주석 제거 및 코드 정리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,120 +3,54 @@ import Browse from './pages/Browse.js';
 import Home from './pages/Home.js';
 
 class App extends React.Component {
-
-  // 조건부 렌더링 (CSR) vs. 조건부 표시 (CSS : display, visibility, overflow, opacity)
-  // React (JavaScript) vs. Vue (directives: v-if, v-show)
-
-  // OTT (구독 === 돈!!!!)
-  // 구독자 (인증, 권한)
-  // 회원가입 → 로그인
-  // 서비스 이용 (display: none; )
-
-  // 선언형
-  // React 처럼 생각하기
   state = {
-    // 사용자가 선언해서 쓰세요.
     headline: 'React Application',
-    // isDisabled: false,
     isPaid: true,
     isToggle: false,
     isLoading: !true,
-    hasError: null, // { message: '서버에서 적절하지 않은 요청이 있었다는 응답이 있었습니다.' },
+    hasError: null,
   };
 
   originalHeadline = this.state.headline;
   willUpdateHeadline = 'NEW HEADLINE! 😃';
 
-  unknown = null;
-
   handleChangeHeadline = () => {
-
-    // const { hasError: error } = this.state;
-
-    // 1. 문
-    // if (error === null || error === undefined) {
-    //   console.log('현재 앱에는 오류(error)가 발생하지 않았습니다.');
-    // }
-
-    // 2. 터너리
-    // (error === null || error === undefined) ? 
-    //   console.log('현재 앱에는 오류(error)가 발생하지 않았습니다.') : 
-    //   null;
-
-    // 3. null 병합 연산자
-    // error ?? console.log('현재 앱에는 오류(error)가 발생하지 않았습니다.');
-  
-    // 옵셔널 체이닝을 사용해 조건 처리해봅니다.
-    // if (error) {
-    //   if (typeof error.log === 'function') {
-    //     error.log();
-    //   }
-    // }
-
-    // error && typeof error.log === 'function' && error.log();
-
-    // error.log?.();
-
-    // let error = {
-    //   log() {
-    //     console.log('this is logger')
-    //   }
-    // }
-
-
-    // 조건 처리
-    // 문을 사용할 것인가?
     if (this.state.isToggle) {
       this.setState({
         isToggle: false,
-        headline: this.originalHeadline
+        headline: this.originalHeadline,
       });
     } else {
       this.setState({
         isToggle: true,
-        headline: this.willUpdateHeadline
+        headline: this.willUpdateHeadline,
       });
     }
-
-    // 아니면 식을 사용할 것인가?
-
-    // this.setState({
-    //   headline: 'NEW HEADLINE! 😃',
-    //   isDisabled: true,
-    // });
-  }
+  };
 
   render() {
+    const { isLoading, isToggle, isPaid, headline, hasError } = this.state;
 
-    const { isToggle, isPaid, headline } = this.state;
-
-    if (this.state.isLoading) {
-      return <div role="alert">데이터 로딩 중...</div> 
+    if (isLoading) {
+      return <div role="alert">데이터 로딩 중...</div>;
     }
 
-    if (this.state.hasError) {
-      return <div role="alert">{this.state.hasError.message}</div>
+    if (hasError) {
+      return <div role="alert">{hasError.message}</div>;
     }
 
-    return (
-      <Home />
-    )
+    return <Home />;
 
     return (
       <div className="App">
         <h1>{headline}</h1>
-        <button
-          // disabled={this.state.isDisabled}
-          type="button" 
-          onClick={this.handleChangeHeadline}
-        >
-          { isToggle ? '오리지널 헤드라인으로 변경' : '뉴 헤드라인으로 변경' }
+        <button type="button" onClick={this.handleChangeHeadline}>
+          {isToggle ? '오리지널 헤드라인으로 변경' : '뉴 헤드라인으로 변경'}
         </button>
 
         <LogIn />
 
-        {/* 조건부 렌더링 (화면에 그리지 않음: 민감, 보안, 결제 등 요구) */}  
-        { isPaid && <Browse /> }
+        {isPaid && <Browse />}
       </div>
     );
   }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,22 @@
 class Home extends React.Component {
   state = {
     products: [
-      { id: 'product-dkfj', name: '웰치스', price: 980, image: 'https://img.danawa.com/prod_img/500000/648/889/img/16889648_1.jpg?shrink=500:500&_v=20221021170923' },
-      { id: 'product-ekdi', name: '두부 1kg', price: 2180, image: 'https://contents.lotteon.com/itemimage/_v105312/LF/14/37/17/3_/0/LF1437173_0_2.jpg/dims/resizef/720X720' },
-    ]
-  }
+      {
+        id: 'product-dkfj',
+        name: '웰치스',
+        price: 980,
+        image:
+          'https://img.danawa.com/prod_img/500000/648/889/img/16889648_1.jpg?shrink=500:500&_v=20221021170923',
+      },
+      {
+        id: 'product-ekdi',
+        name: '두부 1kg',
+        price: 2180,
+        image:
+          'https://contents.lotteon.com/itemimage/_v105312/LF/14/37/17/3_/0/LF1437173_0_2.jpg/dims/resizef/720X720',
+      },
+    ],
+  };
 
   render() {
     const { products } = this.state;
@@ -36,7 +48,7 @@ class Home extends React.Component {
           </li>
         </ul>
       </React.Fragment>
-    )
+    );
   }
 }
 
