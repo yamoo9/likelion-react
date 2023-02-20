fc2be4d23f7fb26e06f4c32564924aecc76166a3
yamoo9 <yamoo9@naver.com>
2023-02-20 16:17:11 +0900

    learn. 카운터 클래스 컴포넌트

--- counter-with-cra/src/app/App.jsx
+++ counter-with-cra/src/app/App.jsx
@@ -1,26 +1,10 @@
 import './App.css';
-
-// 이미지 에셋 불러오기
-import logo from '../assets/logo.svg';
+import Counter from '../components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="React" />
-        <p>
-          <code>src/App.js</code> 파일을 수정하면{' '}
-          <abbr title="Hot Module Replacement">HMR</abbr>, Live Reload 됩니다.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React 배우러 가즈아~~~
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
---
+++ counter-with-cra/src/components/Counter.jsx
@@ -0,0 +1,52 @@
+import { Component } from 'react';
+
+class Counter extends Component {
+  static defaultProps = {
+    count: 1,
+    min: 1,
+    max: 10,
+    step: 1,
+  };
+
+  state = {
+    count: this.props.count,
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div data-component="Counter">
+        <button
+          type="button"
+          onClick={this.handleInc}
+          aria-label="카운트 1 증가"
+        >
+          +
+        </button>
+        <output>{count}</output>
+        <button
+          type="button"
+          onClick={this.handleDec}
+          aria-label="카운트 1 감소"
+        >
+          -
+        </button>
+      </div>
+    );
+  }
+
+  handleInc = () => {
+    this.setState({
+      count: this.state.count + this.props.step,
+    });
+  };
+
+  handleDec = () => {
+    this.setState({
+      count: this.state.count - this.props.step,
+    });
+  };
+}
+
+export default Counter;
