5e686a2ffd0106451be7652a82bf45c40e203bd6
yamoo9 <yamoo9@naver.com>
2023-02-22 10:19:15 +0900

    learn. 이벤트 구독 해제
    - props, state 말고!
    - 클래스 인스턴스 멤버 변수 활용!

--- counter-with-cra/src/components/LifeCycle/LifeCycle.jsx
+++ counter-with-cra/src/components/LifeCycle/LifeCycle.jsx
@@ -6,6 +6,12 @@ const API_ENDPOINT = 'https://randomuser.me/api/?results=5';
 
 class LifeCycle extends React.Component {
   /* 
+  클래스 필드 ------------------------------------------------------------------- */
+
+  // this.clearId
+  clearId = -1;
+
+  /* 
   render 단계 ---------------------------------------------------------------- */
 
   state = {
@@ -60,7 +66,6 @@ class LifeCycle extends React.Component {
         data: data.results,
       });
     } catch (error) {
-      console.log(error);
       this.setState({
         error: {
           message: error.message,
@@ -80,7 +85,7 @@ class LifeCycle extends React.Component {
     //    접속 중인 친구의 온라인 상태 여부 감지하는 이벤트 함수 연결 해제(취소)
 
     // 타이머(특정 주기마다 확인하는 이벤트 함수 시뮬레이션)
-    setInterval(() => {
+    this.clearId = setInterval(() => {
       console.log('친구야 접속 중이니?');
     }, 1500);
 
@@ -119,6 +124,9 @@ class LifeCycle extends React.Component {
   // 구독 중인 이벤트 취소(unsubscription)
   componentWillUnmount() {
     console.log('컴포넌트 언마운트 전에 실행됩니다.');
+
+    clearInterval(this.clearId);
+    console.log('구독 중인 친구 접속 감지 이벤트를 해제했습니다.');
   }
 }
 
