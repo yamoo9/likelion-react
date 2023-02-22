ead0096f58883d9c184003fad89d1bf6945b1be6
yamoo9 <yamoo9@naver.com>
2023-02-22 11:18:19 +0900

    learn. 클래스 인스턴스 멤버 vs. 스태틱(클래스) 멤버

--- counter-with-cra/src/components/LifeCycle/LifeCycle.jsx
+++ counter-with-cra/src/components/LifeCycle/LifeCycle.jsx
@@ -131,3 +131,27 @@ class LifeCycle extends React.Component {
 }
 
 export default LifeCycle;
+
+class ES_Class extends React.Component {
+  // this.myVariable
+  myVariable = '나(this)의 멤버(지역) 변수여~';
+
+  // static member no!! no!!
+  static method() {
+    // console.log(this); // static에서는 this를 사용할 수 없다.
+  }
+
+  state = {};
+
+  render() {
+    return <div lang="en">ECMAScript Class Syntax</div>;
+  }
+
+  otherMethod() {
+    console.log(this.myVariable);
+  }
+
+  anotherMethod() {
+    console.log(this.myVariable);
+  }
+}
