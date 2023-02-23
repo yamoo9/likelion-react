3a04f31feb7196f672b4e3207f87661fdbbfc1a9
yamoo9 <yamoo9@naver.com>
2023-02-23 15:37:21 +0900

    learn. 로그인 페이지 생성
    - 레이아웃 재사용

--- stateless-component/src/app/App.jsx
+++ stateless-component/src/app/App.jsx
@@ -1,9 +1,10 @@
 import Home from '@/pages/Home';
+import SignIn from '@/pages/SignIn';
 
 function App() {
   return (
     <div>
-      <Home />
+      <SignIn />
     </div>
   );
 }
---
+++ stateless-component/src/pages/SignIn.jsx
@@ -0,0 +1,10 @@
+import classes from '@/styles/SignIn.module.css';
+import { BaseLayout } from '@/components';
+
+export default function SignIn() {
+  return (
+    <BaseLayout>
+      <h2>로그인 페이지</h2>
+    </BaseLayout>
+  );
+}
