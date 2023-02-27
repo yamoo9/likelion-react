bd8a2058ab9f461e57126a2abbd0c758f17a2fa6
yamoo9 <yamoo9@naver.com>
2023-02-27 16:46:54 +0900

    learn. Firebase 웹 앱에 연결(추가)

---
+++ firebase-auth/src/firebase/app.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+
+const {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_MESSAGE_SENDER_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_APP_ID,
+} = import.meta.env;
+
+const firebaseConfig = {
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGE_SENDER_ID,
+  appId: VITE_APP_ID,
+};
+
+console.log(firebaseConfig);
+
+export const app = initializeApp(firebaseConfig);
--- firebase-auth/src/main.jsx
+++ firebase-auth/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '@/styles/global.scss';
 import App from '@/app/App';
+import { app } from './firebase/app';
+
+console.log(app);
 
 createRoot(document.getElementById('root')).render(
   <>
