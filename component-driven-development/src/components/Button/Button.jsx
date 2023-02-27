1630e1b9241796b0a5e84d3737faec05e2cd6030
yamoo9 <yamoo9@naver.com>
2023-02-27 10:06:31 +0900

    learn. Button 컴포넌트 비활성 상태 구현

--- component-driven-development/src/components/Button/Button.jsx
+++ component-driven-development/src/components/Button/Button.jsx
@@ -5,17 +5,30 @@ import classes from './Button.module.scss';
 /* Component ---------------------------------------------------------------- */
 
 export function Button({ secondary: isSecondary, disabled, ...restProps }) {
+  let buttonStyle = {};
+
+  if (!disabled) {
+    buttonStyle = {
+      backgroundColor: !isSecondary
+        ? getColor('primary/400')
+        : getColor('white'),
+      color: !isSecondary ? getColor('white') : getColor('primary/400'),
+    };
+  } else {
+    buttonStyle = {
+      backgroundColor: !isSecondary
+        ? getColor('primary/50')
+        : getColor('gray/50'),
+      color: !isSecondary ? getColor('primary/400') : getColor('gray/300'),
+    };
+  }
+
   return (
     <button
       type="button"
       disabled={disabled}
       className={classes.Button}
-      style={{
-        backgroundColor: !isSecondary
-          ? getColor('primary/400')
-          : getColor('white'),
-        color: !isSecondary ? getColor('white') : getColor('primary/400'),
-      }}
+      style={buttonStyle}
       {...restProps}
     />
   );
