e4bb7fd7b9bd3a98134625781767a16d69dd182a
yamoo9 <yamoo9@naver.com>
2023-02-27 13:04:49 +0900

    config. Layout 컴포넌트
    - 레이블 추가
    - 내비게이션 리스트 추가

--- component-driven-development/src/components/Layout/Layout.jsx
+++ component-driven-development/src/components/Layout/Layout.jsx
@@ -1,30 +1,38 @@
-import { node, string } from "prop-types"
-import { Header, Footer, Container } from "@/components"
-import classes from "./Layout.module.scss"
+import { node, string } from 'prop-types';
+import { getPathFromBaseUrl as baseURL } from '@/utils';
+import { Header, Footer, Container } from '@/components';
+import classes from './Layout.module.scss';
 
 /* Component ---------------------------------------------------------------- */
 
+const navList = [
+  { id: 'home', to: baseURL(), text: '홈', active: false },
+  { id: 'signup', to: baseURL('signup'), text: '회원가입', active: true },
+  { id: 'signin', to: baseURL('signin'), text: '로그인', active: false },
+  { id: 'todos', to: baseURL('todos'), text: '할 일 목록', active: false },
+];
+
 export function BaseLayout({ className, children, ...restProps }) {
-  const combineClassNames = `${classes.BaseLayout} ${className}`.trim()
+  const combineClassNames = `${classes.BaseLayout} ${className}`.trim();
 
   return (
     <div className={combineClassNames} {...restProps}>
-      <Header />
+      <Header logoLabel="Stateful Component & Form Design" navList={navList} />
       <main>
         <Container>{children}</Container>
       </main>
       <Footer />
     </div>
-  )
+  );
 }
 
 /* Props -------------------------------------------------------------------- */
 
 BaseLayout.defaultProps = {
-  className: "",
-}
+  className: '',
+};
 
 BaseLayout.propTypes = {
   className: string,
   children: node,
-}
+};
