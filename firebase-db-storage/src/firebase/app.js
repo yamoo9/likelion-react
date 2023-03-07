1cb87e831e09ec710fdda9fdf6c300caaabfaa17
yamoo9 <yamoo9@naver.com>
2023-03-07 10:22:34 +0900

    learn. Firebase 업로드 Part 2.

--- firebase-db-storage/src/firebase/app.js
+++ firebase-db-storage/src/firebase/app.js
@@ -1,21 +1,30 @@
 import { initializeApp } from 'firebase/app';
 
-const {
-  VITE_API_KEY,
-  VITE_AUTH_DOMAIN,
-  VITE_PROJECT_ID,
-  VITE_MESSAGE_SENDER_ID,
-  VITE_STORAGE_BUCKET,
-  VITE_APP_ID,
-} = import.meta.env;
+// const {
+//   VITE_API_KEY,
+//   VITE_AUTH_DOMAIN,
+//   VITE_PROJECT_ID,
+//   VITE_MESSAGE_SENDER_ID,
+//   VITE_STORAGE_BUCKET,
+//   VITE_APP_ID,
+// } = import.meta.env;
+
+// const firebaseConfig = {
+//   apiKey: VITE_API_KEY,
+//   authDomain: VITE_AUTH_DOMAIN,
+//   projectId: VITE_PROJECT_ID,
+//   storageBucket: VITE_STORAGE_BUCKET,
+//   messagingSenderId: VITE_MESSAGE_SENDER_ID,
+//   appId: VITE_APP_ID,
+// };
 
 const firebaseConfig = {
-  apiKey: VITE_API_KEY,
-  authDomain: VITE_AUTH_DOMAIN,
-  projectId: VITE_PROJECT_ID,
-  storageBucket: VITE_STORAGE_BUCKET,
-  messagingSenderId: VITE_MESSAGE_SENDER_ID,
-  appId: VITE_APP_ID,
+  apiKey: 'AIzaSyCL4Rt0HXA-atO8riWJ7vQtsToHGQ-76Sk',
+  authDomain: 'yamoo9-firebase.firebaseapp.com',
+  projectId: 'yamoo9-firebase',
+  storageBucket: 'yamoo9-firebase.appspot.com',
+  messagingSenderId: '425358474331',
+  appId: '1:425358474331:web:20be14942fbfecf4ea137e',
 };
 
 export const app = initializeApp(firebaseConfig);
--- firebase-db-storage/src/pages/UploadFiles/UploadFilesPage.jsx
+++ firebase-db-storage/src/pages/UploadFiles/UploadFilesPage.jsx
@@ -1,29 +1,56 @@
 import { useId } from 'react';
 import { A11yHidden } from '@/components';
 import styled from 'styled-components/macro';
-import { useUploadFiles } from '@/firebase/storage';
+import {
+  useAllDownloadURL,
+  useDeleteFile,
+  useUploadFiles,
+} from '@/firebase/storage';
 
 export default function UploadFilesPage() {
   const id = useId();
 
-  useUploadFiles();
+  const { fileInputRef, uploadFiles } = useUploadFiles();
+
+  // const { isLoading, error, downloadURL } = useDownloadURL('assets/js.png');
+  const { isLoading, error, downloadURLList } = useAllDownloadURL();
+  console.log({ isLoading, error, downloadURLList });
+
+  const { deleteFile } = useDeleteFile();
 
   const handleUploadFiles = () => {
     console.log('파일 업로드 요청');
+    uploadFiles();
+  };
+
+  const handleDeleteFile = () => {
+    console.log('assets/dev.png 삭제');
+    deleteFile('assets/dev.png');
   };
 
   return (
-    <StyledContainer>
-      <div>
-        <A11yHidden as="label" htmlFor={id}>
-          파일 업로드
-        </A11yHidden>
-        <input type="file" id={id} />
-      </div>
-      <button type="button" onClick={handleUploadFiles}>
-        업로드
-      </button>
-    </StyledContainer>
+    <>
+      <StyledContainer>
+        <div>
+          <A11yHidden as="label" htmlFor={id}>
+            파일 업로드
+          </A11yHidden>
+          <input type="file" id={id} multiple ref={fileInputRef} />
+        </div>
+        <button type="button" onClick={handleDeleteFile}>
+          업로드
+        </button>
+      </StyledContainer>
+      {/* <p>파일 업로드 갯수: {urlList ? urlList.length : 0}</p> */}
+      <ul>
+        {downloadURLList &&
+          downloadURLList.map((url) => (
+            <li key={url}>
+              <img height={30} src={url} alt="" />
+            </li>
+          ))}
+      </ul>
+    </>
   );
 }
 
