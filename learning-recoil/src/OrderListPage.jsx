6f8523d76e8a9c0c031c5654843d3a1bcfc0aaae
Yujin <jyj15938@gmail.com>
2023-03-08 13:38:12 +0900

    order list

--- learning-recoil/src/OrderListPage.jsx
+++ learning-recoil/src/OrderListPage.jsx
@@ -1,7 +1,28 @@
+import { useRecoilValue } from 'recoil';
+import { orderListState } from './@store/orderListState';
+
 export default function OrderListPage() {
+  const orderList = useRecoilValue(orderListState);
+
   return (
     <div>
       <h2>주문 목록</h2>
+      <ul>
+        {orderList.map(({ id, order, price }) => (
+          <li key={id}>
+            <h3>{order}</h3>
+            <span>{currencyKR(price)}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+const numberWithComma = (n) => {
+  return n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+};
+
+const currencyKR = (value) => {
+  return `${numberWithComma(value)}원`;
+};
