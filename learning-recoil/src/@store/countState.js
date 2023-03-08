9c023a9605efcd3d9530e786b26405abc5d16a21
yamoo9 <yamoo9@naver.com>
2023-03-08 14:40:00 +0900

    learn. 아톰 이펙트
    - 리액트의 useEffect와 유사

--- learning-recoil/src/@store/countState.js
+++ learning-recoil/src/@store/countState.js
@@ -1,6 +1,37 @@
 import { atom } from 'recoil';
 
+// atom (state)
+// export const countState = atom({
+//   key: 'countState',
+//   default: 0,
+// });
+
+const persistEffect =
+  (key) =>
+  ({ onSet, setSelf }) => {
+    const count = localStorage.getItem(key);
+
+    if (count) {
+      setSelf(JSON.parse(count));
+    }
+
+    onSet((newValue, _, isReset) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
+// atom (state + effects)
 export const countState = atom({
   key: 'countState',
   default: 0,
+  effects: [
+    persistEffect('count'),
+    ({ onSet }) => {
+      onSet((newValue) => {
+        console.log(newValue);
+      });
+    },
+  ],
 });
