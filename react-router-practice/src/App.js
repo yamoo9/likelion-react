282dc334304634d522db3ae336fec6da2d496760
yamoo9 <yamoo9@naver.com>
2023-03-08 17:42:30 +0900

    learn. localStorage + Change Theme
    - Recoil : Atom + Atom Effects
    - useLayoutEffect

--- react-router-practice/src/App.js
+++ react-router-practice/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useLayoutEffect } from 'react';
 import {
   RouterProvider,
   createBrowserRouter,
@@ -11,6 +11,7 @@ import {
 
 import { loader as postListLoader } from './PostListPage';
 import { loader as postDetailLoader } from './PostDetailPage';
+import { atom, useRecoilState } from 'recoil';
 
 /* Pages -------------------------------------------------------------------- */
 
@@ -58,12 +59,62 @@ router = createBrowserRouter(
 
 /* App ---------------------------------------------------------------------- */
 
+const darkModeState = atom({
+  key: 'darkModeState',
+  default: false,
+  effects: [
+    ({ onSet, setSelf }) => {
+      onSet((newValue) => {
+        console.log('updated darkModeState atom value:', newValue);
+      });
+    },
+  ],
+});
+
 function App() {
+  const [darkMode, setDarkMode] = useRecoilState(darkModeState);
+
+  useLayoutEffect(() => {
+    handleReadDarkMode();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleReadDarkMode = () => {
+    const darkModeLocal = localStorage.getItem('darkMode');
+    if (darkModeLocal) {
+      setDarkMode(JSON.parse(darkModeLocal));
+      console.log(`READ: ${darkModeLocal}`);
+    }
+  };
+
+  const handleToggleDarkMode = () => {
+    const updateDarkMode = !darkMode;
+    setDarkMode(updateDarkMode);
+    localStorage.setItem('darkMode', JSON.stringify(updateDarkMode));
+    console.log(`WRITE: ${updateDarkMode}`);
+  };
+
   return (
-    <div className="App">
-      <Suspense fallback={<Loading />}>
+    <div
+      className="App"
+      style={{
+        minHeight: '100vh',
+        padding: 40,
+        background: darkMode ? '#000' : '#fff',
+        color: !darkMode ? '#000' : '#fff',
+      }}
+    >
+      <h2>Theme Changer</h2>
+      <button type="button" onClick={handleReadDarkMode}>
+        READ DARK MODE
+      </button>
+      <button type="button" onClick={handleToggleDarkMode}>
+        TOGGLE DARK MODE
+      </button>
+
+      {/* <Suspense fallback={<Loading />}>
         <RouterProvider router={router} />
-      </Suspense>
+      </Suspense> */}
     </div>
   );
 }
